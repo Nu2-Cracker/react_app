d36c4fcbcff649d4d2bac771f13ac5f25002073b
上原涼慈 <uehararyouji@uehararyoujinoMacBook-Pro.local>
2021-01-31 23:52:47 +0900

    cmd

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ docker build -t react_env:myself . --no-cache
 # docker run -it --rm --name react_env react_env:myself /bin/bash
 
 #開発用
-docker run -it -d -v ./react_app:/react_app --name react_env -p 8001:8001  react_env:myself
+app_dir=`pwd`
+docker run -it -d -v $app_dir:/react_app --name react_env -p 8001:8001  react_env:myself
 
 docker exec -it react_env bash
 
