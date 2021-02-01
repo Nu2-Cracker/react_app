5baf874527610df25fe200bde8b5605de795698d
Ryoji Uehara <orcinusorca1758dv6932@gmail.com>
2021-02-01 10:00:35 +0900

    "components"

--- App.jsx
+++ App.jsx
@@ -1,21 +1,64 @@
 
-import React, { Component } from 'react';
-class App extends Component {
-    render(){//登録できるコンポーネントは一つかな
-        // コンポーネント内でのコメントは、{/*内容*/}
-        var i = 0;
-        var myStyle = {
-            fontSize: 100,
-            color: '#FF0000'
+import React from 'react';
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            data:
+            [
+                {
+                    "id":1,
+                    "name":"Foo",
+                    "age":"20"
+                 },
+                 {
+                    "id":2,
+                    "name":"Bar",
+                    "age":"30"
+                 },
+                 {
+                    "id":3,
+                    "name":"Baz",
+                    "age":"40"
+                 }
+            ]
         }
+    }
+    render(){
         return(
             <div>
-                <h1 style={myStyle}>Header</h1>
-                {/*Multi line comment...*/}
-                {/*comment2*/}
+                <Header/>
+                <table>
+                    <tbody>
+                        {this.state.data.map((person, i)=><TableRow key={i}
+                        data={person}/>)}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Header</h1>
             </div>
         );
     }
 }
 
+class TableRow extends React.Component{
+    render(){
+        return(
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
+        );
+    }
+
+}
 export default App;
---
+++ commit.sh
@@ -0,0 +1,7 @@
+echo -n \"commit message:\"
+read c_m
+
+git add .
+git commit -m \"$c_m\"
+git pull origin main
+git push origin main
